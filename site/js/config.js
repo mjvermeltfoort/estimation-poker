c8fb7fa3a7704f0c7b57293798151cb44fb9cbe4
Mark Vermeltfoort <mjvermeltfoort@gmail.com>
2026-07-22 22:29:54 +0200

    Update API URL in configuration

--- site/js/config.js
+++ site/js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbyTJTFiuct48J2vCf5tVCkx5J7MmXcUYOgzpcQoiVW4M1CX9Pc8y7Y2Xg2OHls2Pikr/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwRtFawUhC6m-fFOWMv5XWd2Nsz3u898oMVIjqAOhgrJQOZiYNgrDNEDHpji24d8As0/exec",
   googleClientId: "848494307751-1nik59q3dh1n6f13f6utejime4b2k5fv.apps.googleusercontent.com",
   pollingIntervalMs: 3000,
   hiddenPollingIntervalMs: 15000,
